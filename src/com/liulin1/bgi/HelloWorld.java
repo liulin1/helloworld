8d584c93fcf11acefd95dcd5fc52e5503fbeb397
刘林(Lin Liu) <liulin1@genomics.cn>
2017-05-02 18:35:12 +0800

    Initial commit

---
+++ src/com/liulin1/bgi/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.liulin1.bgi;
+
+/**
+ * Created by liulin1 on 2017/5/2.
+ */
+public class HelloWorld {
+
+        public static void main(String[] args){
+            System.out.println("Hello World");
+        }
+}
